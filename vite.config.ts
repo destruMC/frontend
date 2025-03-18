d91ff8922d2d90562d33d46db90c4002d9f84710
KKW557 <kevin557557@gmail.com>
2025-03-19 01:15:25 +0800

    1.3.2

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
